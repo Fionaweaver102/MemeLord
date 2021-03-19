c1e6873725954f7716bbab6e45e6a24bcfe3d3e4
Fiona Weaver <fiona.l.weaver@gmail.com>
2021-03-19 13:36:13 -0600

    Get Memes from database onto Index page.

---
+++ FrontEnd/src/Index.js
@@ -0,0 +1,44 @@
+const endPoint = "http://localhost:3000/memes"
+
+const memes = [];
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  document.getElementById('render-list').innerHTML = '';
+  fetch(endPoint)
+    .then(response => response.json())
+    .then(memes => {
+      append(memes);
+    })
+}
+
+function append(memes) {
+  let ul = document.createElement('ul');
+  ul.setAttribute('id', 'meme-list');
+  document.getElementById('render-list').appendChild(ul);
+
+  for (let i = 0; i < memes.length; i++) {
+    let meme = memes[i];
+    let img = document.createElement('img');
+    img.src = meme.image_url;
+    let p = document.createElement("p");
+    p.setAttribute('class', 'text-lg leading-6 font-medium text-gray-900 px-4 py-5 sm:px-6');
+    p.innerText = meme.title;
+
+    createList(ul, img, p, meme);
+  }
+}
+
+function createList(ul, img, p, meme) {
+  let li = document.createElement("li");
+
+  li.setAttribute('class', 'bg-white shadow overflow-hidden sm:rounded-lg mt-8');
+  li.append(img);
+  li.append(p);
+  ul.append(li);
+}
+
+// function make(element) {
+//   return document.createElement(element)
+// }
